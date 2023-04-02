9033b098169b1b7940499f3d2c81accae236a529
mansourhassani <mansour.hasani.dev@gmail.com>
2023-04-02 19:45:38 +0330

    Adding the function to change the status of the password to visible and invisible.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { FaEye } from "react-icons/fa";
+import { useState } from "react";
+
+import { Icon } from "react-icons-kit";
+import { eye } from "react-icons-kit/feather/eye";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+
 function App() {
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eye);
+
+  const handleClick = () => {
+    if (type === "password") {
+      setType("text");
+      setIcon(eyeOff);
+    } else {
+      setType("password");
+      setIcon(eye);
+    }
+  };
+
   return (
     <div className="wrapper">
       <div className="input-fields">
-        <span>
-          <FaEye />
+        <span onClick={handleClick}>
+          <Icon icon={icon} size={25} />
         </span>
-        <input type="password" className="password" />
+        <input type={type} />
       </div>
     </div>
   );
